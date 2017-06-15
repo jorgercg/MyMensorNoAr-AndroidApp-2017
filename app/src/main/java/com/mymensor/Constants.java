fd770889f66d592c24d91e6955848975928653f4
Jorge Goncalves <jrcgonc@gmail.com>
2017-06-15 23:24:25 +0100

    Commit MyMensorAR OpenCV version 10001 15jun2017 23h24min

--- app/src/main/java/com/mymensor/Constants.java
+++ app/src/main/java/com/mymensor/Constants.java
@@ -23,7 +23,7 @@ public class Constants {
     public static final int cameraHeigthInPixels = 720;
     public static final int xAxisTrackingCorrection = 440; // (1280-400)/2=440 // (1280-700)/2=290
     public static final int yAxisTrackingCorrection = 160; // (720-400)/2=160 // (720-700)/2=10
-    public static final int[] validIdMarkersForMyMensor = { 10,11,12,13,14,15,17,18,19,
+    public static final int[] validIdMarkersForMyMensor = { 10,11,12,13,14,15,16,17,18,19,
                                                             20,21,22,23,24,25,26,27,28,29,
                                                             30,31,32,33,34,35,36,37,38,39 };
 
